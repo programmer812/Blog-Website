b8bcc4508ad7daa687de498a0600b162a96a1d22
programmer812 <64325806+programmer812@users.noreply.github.com>
2022-08-29 22:17:30 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(mongoDBAuth.mongoDBAuth);
+mongoose.connect(String(mongoDBAuth.mongoDBAuth));
 
 const postSchema = {
   title: String,
