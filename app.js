de95fad16b019f695cd70ae8e590912a51cc3c6b
programmer812 <64325806+programmer812@users.noreply.github.com>
2022-08-29 18:15:18 -0600

    Updated blog website to not store blog posts in a database

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const { PORT = 3000 } = process.env;
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,22 +17,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+mongoose.connect(process.env.MONGODB_AUTH);
+
+const postSchema = {
+  title: String,
+  content: String
+}
+
+const Post = mongoose.model("Post", postSchema); // collection of documents
 
 app.get("/", (req, res) => {
 
-  res.render("home", {
-    homeContent: homeStartingContent,
-    postsArr: posts
+  Post.find({}, (err, posts) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        homeContent: homeStartingContent,
+        postsArr: posts
+      });
+    }
   });
 })
 
-app.get("/posts/:postName", (req, res) => {
+app.get("/posts/:postId", (req, res) => {
   
-  const postName = _.lowerCase(req.params.postName);
+  const postId = req.params.postId;
 
-  posts.forEach((post) => {
-    if (postName === _.lowerCase(post.title)) {
+  Post.findById(postId, (err, post) => {
+    if (err) {
+      console.log(err);
+    } else {
       res.render("post", {
         post: post
       });
@@ -61,16 +79,20 @@ app.post("/compose", (req, res) => {
   const postTitle = req.body.postTitle;
   const postBody = req.body.postBody;
 
-  const post = {
+  const post = new Post({
     title: postTitle,
     content: postBody
-  };
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
+  post.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
 })
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log(`Server started on port ${PORT}`);
 });
